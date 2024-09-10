f5a861d7ef9af44cea1806e19d30adf824f62cf1
Ramesh Allamsetti <iamramesh.a@gmail.com>
2024-09-10 07:20:18 +0530

    New files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+console.log("hiii")
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8080"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+require("./app/routes/tutorial.routes.js")(app);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
